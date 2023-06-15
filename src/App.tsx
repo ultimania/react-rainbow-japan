734dd20aa5a1b63e974be474f305dd9e2a6aeed2
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-06-15 13:46:49 +0000

    feat: add layout tag

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layout-header"></div>
+      <div className="layout-main-view"></div>
+      <div className="layout-service"></div>
+      <div className="layout-achiebement"></div>
+      <div className="layout-notification"></div>
+      <div className="layout-footer"></div>
     </div>
   )
 }
