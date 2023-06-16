a1f27648af6d90166124892144674d6ff4445283
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-06-16 03:14:10 +0000

    feat: main contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import siteLogo from './siteLogo.svg'
 import imgMv1 from './img_mv_01.jpg'
 import imgMv2 from './img_mv_02.jpg'
@@ -6,45 +6,53 @@ import imgMv3 from './img_mv_03.jpg'
 import './App.css'
 
 function App() {
+  interface Slide {
+    id: number
+    imgSrc: string
+    alt: string
+  }
+
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const handleDotClick = (index: number) => {
+    setCurrentSlide(index)
+  }
+
+  const slides = [
+    { id: 1, imgSrc: imgMv1, alt: 'img-mv1' },
+    { id: 2, imgSrc: imgMv2, alt: 'img-mv2' },
+    { id: 3, imgSrc: imgMv3, alt: 'img-mv3' },
+  ]
+
   return (
     <div className="App">
-      <div className="layout-header">
-        <header className="header">
-          <img src={siteLogo} className="site-logo" alt="logo" />
-          <div className="navigation">
-            <ul className="nav-area">
-              <li className="nav-item">特徴</li>
-              <li className="nav-item">サービス</li>
-              <li className="nav-item">制作実績</li>
-              <li className="nav-item">コラム</li>
-              <li className="nav-item">会社情報</li>
-              <li className="nav-item">採用情報</li>
-              <li className="nav-item">お知らせ</li>
+      <div id="layout-main-view">
+        <div className="main">
+          <div className="main-theme">
+            <h1>「つたえる」をデザインする。</h1>
+            <h2>Web Marketing x Information Design</h2>
+          </div>
+          <div className="main-slideshow">
+            <ul className="slideshow">
+              {slides.map((slide, index) => (
+                <li
+                  key={slide.id}
+                  className={`slide ${index === currentSlide ? 'active' : ''}`}
+                >
+                  <img src={slide.imgSrc} alt={slide.alt} />
+                </li>
+              ))}
             </ul>
+            <div className="paging">
+              {slides.map((_, index) => (
+                <div
+                  key={index}
+                  className={`dot ${index === currentSlide ? 'active' : ''}`}
+                  onClick={() => handleDotClick(index)}
+                />
+              ))}
+            </div>
           </div>
-          <div className="contact">
-            <span className="icon-label mail">ご相談・お問い合わせ</span>
-          </div>
-        </header>
-      </div>
-
-      <div className="layout-main-view">
-        <div className="main-theme">
-          <h1>「つたえる」をデザインする。</h1>
-          <h2>Web Marketing x Information Design</h2>
-        </div>
-        <div className="main-slideshow">
-          <ul className="slideshow">
-            <li className="slide">
-              <img src={imgMv1} alt="img-mv1"></img>
-            </li>
-            <li className="slide">
-              <img src={imgMv2} alt="img-mv2"></img>
-            </li>
-            <li className="slide">
-              <img src={imgMv3} alt="img-mv3"></img>
-            </li>
-          </ul>
         </div>
         <div className="company-description">
           レインボー・ジャパンは、webサイト、インターネットメディアを活用し、企業課題を解決するコンサルティング・webサイト制作会社です。
@@ -54,13 +62,35 @@ function App() {
         </div>
       </div>
 
-      <div className="layout-service"></div>
+      <div id="layout-service"></div>
 
-      <div className="layout-achiebement"></div>
+      <div id="layout-achiebement"></div>
 
-      <div className="layout-notification"></div>
+      <div id="layout-notification"></div>
 
-      <div className="layout-footer"></div>
+      <div id="layout-footer"></div>
+
+      <div id="layout-header">
+        <header className="header">
+          <div className="site-logo-area">
+            <img src={siteLogo} className="site-logo" alt="logo" />
+          </div>
+          <div className="nav-area">
+            <ul className="nav-main">
+              <li className="nav-item">特徴</li>
+              <li className="nav-item">サービス</li>
+              <li className="nav-item">制作実績</li>
+              <li className="nav-item">コラム</li>
+              <li className="nav-item">会社情報</li>
+              <li className="nav-item">採用情報</li>
+              <li className="nav-item">お知らせ</li>
+            </ul>
+            <div className="contact">
+              <span className="icon-label mail">ご相談・お問い合わせ</span>
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   )
 }
