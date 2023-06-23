023c504b60f8b412164b4b1481e11b393d0cff14
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-06-23 14:12:32 +0000

    feat: slide show animation

--- src/components/Elements/ImageView/SlideShow.tsx
+++ src/components/Elements/ImageView/SlideShow.tsx
@@ -1,45 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 type SlideShowProps = {
-  items: SlideShowItem[];
-};
+  items: SlideShowItem[]
+}
 
 type SlideShowItem = {
-  src: string;
-};
+  src: string
+}
 
 export const SlideShow = (props: SlideShowProps) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideInterval = 5600;
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % props.items.length)
+    }, slideInterval)
+    return () => clearInterval(interval)
+  }, [props.items.length])
 
   const handleDotClick = (index: number) => {
-    setCurrentSlide(index);
-  };
+    setCurrentSlide(index)
+  }
 
   const renderSlides = () => {
     return props.items.map((slide, index) => (
       <li
         key={index}
-        className={`slide ${index === currentSlide ? 'active' : ''}`}
+        className={`slide${index === currentSlide ? ' active' : ''}`}
       >
         <img src={slide.src} alt={index.toString()} />
       </li>
-    ));
-  };
+    ))
+  }
 
   const renderDots = () => {
     return props.items.map((_, index) => (
       <div
         key={index}
-        className={`dot ${index === currentSlide ? 'active' : ''}`}
+        className={`dot${index === currentSlide ? ' active' : ''}`}
         onClick={() => handleDotClick(index)}
       />
-    ));
-  };
+    ))
+  }
 
   return (
     <div className="main-slideshow">
       <ul className="slideshow">{renderSlides()}</ul>
       <div className="paging">{renderDots()}</div>
     </div>
-  );
-};
+  )
+}
