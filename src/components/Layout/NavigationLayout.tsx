5cfb94c7cd14a594d3f166ab40d7446e4927f91d
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-06-28 23:44:41 +0000

    fix: navigation layout for responsive

--- src/components/Layout/NavigationLayout.tsx
+++ src/components/Layout/NavigationLayout.tsx
@@ -2,44 +2,44 @@ import React, { useEffect, useState } from 'react'
 import { NavigationList } from '../Elements/List'
 import { Button } from '../Elements/Button'
 import { IconEnum } from '../Types'
-import siteLogo from '../../assets/siteLogo.svg';
+import siteLogo from '../../assets/siteLogo.svg'
 import styleModule from './NavigationLayout.module.scss'
 
-import {
-  headerNavItems,
-} from '../..//data';
+import { headerNavItems } from '../..//data'
 
 export const NavigationLayout = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
-      setIsScrolled(scrollTop > 0);
-    };
+      const scrollTop = window.scrollY || document.documentElement.scrollTop
+      setIsScrolled(scrollTop > 0)
+    }
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
-    <div id={styleModule["layout-header"]}>
-      <header className={styleModule["header"]}>
-        <div className={styleModule["site-logo-area"]}>
-          <img src={siteLogo} className={styleModule["site-logo"]} alt="logo" />
+    <div id={styleModule['layout-header']}>
+      <header className={styleModule['header']}>
+        <div className={styleModule['site-logo-area']}>
+          <img src={siteLogo} className={styleModule['site-logo']} alt="logo" />
         </div>
-        <div className={styleModule["nav-area"]}>
+        <div className={styleModule['nav-area']}>
           <NavigationList items={headerNavItems} drawer />
-          <Button
-            minimum={isScrolled}
-            icon={IconEnum.Mail}
-            href="/contact"
+          <div
+            className={`${styleModule['contact']} ${
+              isScrolled ? styleModule['is-scrolled'] : ''
+            }`}
           >
-            Consultation and Contact
-          </Button>
+            <Button minimum={isScrolled} icon={IconEnum.Mail} href="/contact">
+              Consultation and Contact
+            </Button>
+          </div>
         </div>
       </header>
     </div>
